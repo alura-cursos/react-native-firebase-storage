277c2f0ff4cf02a9eb391a7b42b5cf6f81ffd292
andreocunha <andreoliveiracunha20@gmail.com>
2022-12-04 14:28:47 -0300

    projeto base

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
-import { getStorage } from "firebase/storage";
 import { API_KEY, AUTH_DOMAIN, PROJECT_ID, STORAGE_BUCKET, MESSAGING_SENDER_ID, APP_ID } from "@env"
 
 const firebaseConfig = {
@@ -12,13 +11,10 @@ const firebaseConfig = {
   appId: APP_ID
 };
 
-// Initialize Firebase
+// Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Firestore
+// Inicializa o Firestore
 const db = getFirestore(app);
 
-// Initialize Storage
-const storage = getStorage(app);
-
-export { db, storage };
+export { db };
