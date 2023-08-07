7f34e166c3c9a1efcaadc84ea72b0a6ca5dfe43f
leandro barbosa <motorola1710@gmail.com>
2023-08-07 14:31:46 -0300

    alte

---
+++ src/servicos/storage.js
@@ -0,0 +1,21 @@
+import { storage } from "../config/firebase";
+import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
+
+export async function salvarImagem(imagem, imagemNome){
+  if(!imagem) return;
+  
+  const downloadImagem = await fetch(imagem)
+  const blobImagem = await downloadImagem.blob()
+
+  const imagemRef = ref(storage, `posts/${imagemNome}.png`)
+
+  try {
+    await uploadBytes(imagemRef, blobImagem);
+    const url = await getDownloadURL(imagemRef);
+    return url;
+  }
+  catch(error) {
+    console.log(error)
+    return null;
+  }
+}
--- src/telas/Post/index.js
+++ src/telas/Post/index.js
@@ -1,11 +1,14 @@
 import { useState } from "react";
-import { View, Text, TextInput, ScrollView, TouchableOpacity } from "react-native";
+import { View, Text, TextInput, ScrollView, TouchableOpacity ,Image} from "react-native";
 import { salvarPost, atualizarPost, deletarPost } from "../../servicos/firestore";
 import estilos from "./estilos";
 import { entradas } from "./entradas";
 import { alteraDados } from "../../utils/comum";
 import { IconeClicavel } from "../../componentes/IconeClicavel";
 
+import { salvarImagem } from "../../servicos/storage";
+
+const imagemGalaxia = "https://img.freepik.com/vetores-gratis/fundo-de-galaxia-em-aquarela-pintado-a-mao_52683-63441.jpg?w=740&t=st=1691416309~exp=1691416909~hmac=7c196aa454bf52ce27c656ae519f94c341230b3411c97d64ba9692af6fd5fc9d"
 
 export default function Post({ navigation, route }) {
     const [desabilitarEnvio, setDesabilitarEnvio] = useState(false);
@@ -14,22 +17,33 @@ export default function Post({ navigation, route }) {
     const [post, setPost] = useState({
         titulo: item?.titulo || "",
         fonte: item?.fonte || "",
-        descricao: item?.descricao || ""
+        descricao: item?.descricao || "",
+        imagemUrl: item?.imagemUrl || null
     });
 
-    async function salvar() {
+   async function salvar() {
         setDesabilitarEnvio(true);
+
+       
+
         if (item) {
             await atualizarPost(item.id, post);
+           navigation.goBack();
         } else {
-            await salvarPost(post);
+           const idpost = await salvarPost({post});
+           navigation.goBack();
+           const url = await salvarImagem(imagemGalaxia, 'galaxia');
+            await atualizarPost(idpost, post),{
+                imagemUrl: url
+            }
         }
-        
-        navigation.goBack();
+     
     }
+
 
     return (
         <View style={estilos.container}>
+      
             <View style={estilos.containerTitulo}>
                 <Text style={estilos.titulo}>{item ? "Editar post" : "Novo Post"}</Text>
                 <IconeClicavel 
